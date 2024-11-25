bbd82a12280857353d1f89cfd51a7c7dcf0bf7e6
Moondouble <jjm0586@sungkyul.ac.kr>
2024-11-25 11:55:29 +0900

    프로젝트 생성

---
+++ miniProject/src/main/java/miniProject/MiniProjectApplication.java
@@ -0,0 +1,20 @@
+package miniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String index() {
+		return "thymeleaf/index";
+	}
+
+}
