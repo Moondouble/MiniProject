7e1e88d21917904acb9acb47401f3a9038e60341
Moondouble <jjm0586@sungkyul.ac.kr>
2024-12-03 17:51:19 +0900

    집에서 해~

--- miniProject/src/main/java/miniProject/domain/GoodsDTO.java
+++ miniProject/src/main/java/miniProject/domain/GoodsDTO.java
@@ -13,20 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GoodsDTO {
-	String goodsNum;
-	String goodsName;
-	Integer goodsPrice;
-	String goodsContents;
-	String empNum;
-	String memberNum;
-	Integer visitCount;
-	Date goodsRegist;
-	String updateEmpNum;
-	Date goodsUpdateDate;
-	
-	// 디비에 파일명을 저장하기 위해 주가합니다.
-	String goodsMainImage;
-	String goodsMainStoreImage;
-	String goodsDetailImage;
-	String goodsDetailStoreImage;
+    String goodsNum;
+    String goodsName;
+    Integer goodsPrice;
+    String goodsContents;
+    String memberNum;
+    Integer visitCount;
+    Date goodsRegist;
+    Date goodsUpdateDate;
+
+    // 디비에 파일명을 저장하기 위해 추가합니다.
+    String goodsMainImage;
+    String goodsMainStoreImage;
+    String goodsDetailImage;
+    String goodsDetailStoreImage;
 }
